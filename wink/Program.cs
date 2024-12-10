2222449ba860b6caea1cbcd58b40377b3d80415c
mohammedaydi <mohammed7.aydi@gmail.com>
2024-12-10 14:44:37 +0200

    autentication process

--- wink/Program.cs
+++ wink/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using wink.Models;
 using wink.Services;
 
@@ -8,13 +11,36 @@ builder.Services.Configure<WinkDatabaseSettings>(builder.Configuration.GetSectio
 
 // Add services to the DI container.
 builder.Services.AddSingleton<UserService>();
-
+//builder.Services.AddSingleton<ItemService>();
+builder.Services.AddSingleton<AuthService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// JWT Authentication configuration
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"], // e.g., "yourdomain.com"
+            ValidAudience = builder.Configuration["Jwt:Audience"], // e.g., "yourdomain.com"
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
+        };
+    });
+
+//build the app
 var app = builder.Build();
+
+// Use Authentication and Authorization middleware --added with jwt
+app.UseAuthentication();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
