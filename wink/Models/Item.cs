d756f16028ce7b4f93ec2fea23c045c55622fed5
mohammedaydi <mohammed7.aydi@gmail.com>
2024-12-10 14:51:38 +0200

    items routes

---
+++ wink/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+namespace wink.Models
+{
+    public class Item
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Title")]
+        public string Name { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Image { get; set; } = null!;
+
+    }
+}
